2818de8f85d7540019ecbadbb93e02d9e7e386c3
LancelotPincet <40608570+LancelotPincet@users.noreply.github.com>
2025-08-17 20:06:06 +0200

    auto-commit

---
+++ libsLP/pyLP/src/pylp/print_LP/print.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author        : Lancelot PINCET
+Email         : lancelot.pincet@free.fr
+Date          : 2025-08-17
+Description   : Changes default print function to add functionalities
+"""
+
+
+
+# %% Libraries
+TODO
+
+
+
+# %% function
+def print(/, *, **kwargs) : -> None
+    '''
+    Changes default print function to add functionalities
+    '''
+    return None
+
+
+
+# %% Class
+class print() :
+    '''
+    Changes default print function to add functionalities
+    '''
+
+    def __init__(self) :
+        pass
+    
+
+
+# %% Main function run
+if __name__ == "__main__":
+    main()
