c2eeb993ba7b548a6e761a7080904a885e4bb970
LancelotPincet <lancelot.pincet@free.fr>
2025-09-16 10:39:15 +0200

    auto-commit

--- src/corelp/modules/Section_LP/Section.py
+++ src/corelp/modules/Section_LP/Section.py
@@ -16,6 +16,7 @@ This class defines decorator instances allowing to create section functions.
 from corelp import print, folder, selfkwargs, kwargsself
 from dataclasses import dataclass, field
 import pickle
+import joblib
 from pathlib import Path
 from functools import wraps
 import hashlib
@@ -29,7 +30,7 @@ import os
 class Section() :
     '''
     This class defines decorator instances allowing to create section functions.
-    Saves results into a folder, if another call occurs, can load-back the precalculated data.
+    Cache results into a folder, if another call occurs, can load-back the precalculated data.
     
     Parameters
     ----------
@@ -58,7 +59,7 @@ class Section() :
     >>> testfunc.print('1+3=', add(1, 3)) # New call with other parameters : crushed previous results with new ones
     >>> testfunc.print('1+3=', add(1, b=3)) # Second call with these parameters : loads precalculated results
     ...
-    >>> @section() # Creates an index of 2
+    >>> @section(cache=False) # Creates an index of 2, does no caching
     ... def sub(a, b=0) :
     ...     return a - b
     ...
@@ -66,7 +67,7 @@ class Section() :
     ... def mul(a, b=0) :
     ...     return a * b
     ...
-    >>> @section() # Creates an index of 11
+    >>> @section(new=True) # Creates an index of 11, always creates new cache
     ... def div(a, b) :
     ...     return a / b
     '''
@@ -85,7 +86,7 @@ class Section() :
             self.parent_path = Path(self.parent_path)
 
     # Decorator
-    def __call__(self, *, new=None, num=None, symlink=None):
+    def __call__(self, *, new=None, num=None, symlink=None, cache=True):
         if new is None :
             new = self.new
         if num is None :
@@ -101,58 +102,62 @@ class Section() :
                 print(f'\n#### {num}. {name.replace("_"," ")} section\n')
 
                 # Creating hash
-                print('**Call hash:**')
-                bound = inspect.signature(func).bind(*args, **kwargs)
-                bound.apply_defaults()
-                serialized = pickle.dumps(bound.arguments)
-                args_hash = hashlib.md5(serialized).hexdigest()
-                result_file = wrapper.path / f'{args_hash}.pkl'
-                print(f'*{args_hash}*\n')
+                if cache :
+                    print('**Call hash:**')
+                    bound = inspect.signature(func).bind(*args, **kwargs)
+                    bound.apply_defaults()
+                    serialized = pickle.dumps(bound.arguments)
+                    args_hash = hashlib.md5(serialized).hexdigest()
+                    result_file = wrapper.path / f'{args_hash}.pkl'
+                    print(f'*{args_hash}*\n')
 
-                # Checking already calculated exists
-                if result_file.exists() and not new :
-                    print('**Loading from *precalculated* results...**')
-                    with open(result_file, 'rb') as f:
-                        result = pickle.load(f)
-                    print('...loaded\n')
+                    # Checking already calculated exists
+                    if result_file.exists() and not new :
+                        print('**Loading from *precalculated* results...**')
+                        with open(result_file, 'rb') as f:
+                            result = joblib.load(f)
+                        print('...loaded\n')
+                        return result
 
-                # New calculations
-                else :
-                    folder(wrapper.path, warning=False)
-                    print('**Calculating results:**')
-                    print_status = kwargsself(print)
-                    print.file = wrapper.path / f'{name}_log.md'
-                    result = func(*args, **kwargs)
-                    selfkwargs(print, print_status)
-                    print('...calculated\n')
+                # Calculations
+                folder(wrapper.path, warning=False)
+                print('**Calculating results:**')
+                print_status = kwargsself(print)
+                print.file = wrapper.path / f'{name}_log.md'
+                result = func(*args, **kwargs)
+                selfkwargs(print, print_status)
+                print('...calculated\n')
+
+                # Caching
+                if cache :
                     print('**Saving results:**')
                     with open(result_file, 'wb') as f:
-                        pickle.dump(result, f)
+                        joblib.dump(result, f)
                     print('...saved\n')
 
-                    # Create symlink
-                    if symlink is not None :
-                        print('**Creating symlinks:**')
-                        for link in symlink :
-                            print(f"- {link}")
-                            link_path = Path(link)
-                            link_folder = self.parent_path / link_path.stem
-                            new_stem = str(self.subfolder.as_posix()).replace('/', '--')
-                            if not link_folder.exists() :
-                                folder(link_folder, warning=False)
-                            link_from = wrapper.path / link_path
-                            link_to = link_folder / f"{new_stem}{link_path.suffix}"
-                            if link_to.exists() or link_to.is_symlink():
-                                link_to.unlink()
-                            if os.name == "nt":
-                                try :
-                                    link_to.symlink_to(link_from, link_from.is_dir())
-                                except OSError :
-                                    print("Windows does not allow to create symlink, aborting. Consider using Windows in Developper mode.")
-                                    break
-                            else:
-                                link_to.symlink_to(link_from)
-                        print('...created\n')
+                # Create symlink
+                if symlink is not None :
+                    print('**Creating symlinks:**')
+                    for link in symlink :
+                        print(f"- {link}")
+                        link_path = Path(link)
+                        link_folder = self.parent_path / link_path.stem
+                        new_stem = str(self.subfolder.as_posix()).replace('/', '--')
+                        if not link_folder.exists() :
+                            folder(link_folder, warning=False)
+                        link_from = wrapper.path / link_path
+                        link_to = link_folder / f"{new_stem}{link_path.suffix}"
+                        if link_to.exists() or link_to.is_symlink():
+                            link_to.unlink()
+                        if os.name == "nt":
+                            try :
+                                link_to.symlink_to(link_from, link_from.is_dir())
+                            except OSError :
+                                print("Windows does not allow to create symlink, aborting. Consider using Windows in Developper mode.")
+                                break
+                        else:
+                            link_to.symlink_to(link_from)
+                    print('...created\n')
 
 
                 return result
